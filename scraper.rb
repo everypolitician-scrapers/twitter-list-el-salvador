68f2b110dd54dc21ba3d3fc1fb171ad3f01dcf75
Tony Bowden <tony@tmtm.com>
2017-08-25 06:50:22 +0100

    update list to diputadas-y-diputados1
    the original list no longer exists

--- scraper.rb
+++ scraper.rb
@@ -4,4 +4,4 @@ require 'twitter_list'
 abort "Need to set ENV['MORPH_TWITTER_TOKENS']" unless ENV.key? 'MORPH_TWITTER_TOKENS'
 twitter_list = TwitterList::Scraper.new(twitter_tokens: ENV['MORPH_TWITTER_TOKENS'])
 
-ScraperWiki.save_sqlite([:id], twitter_list.people('AsambleaSV', 'diputadas-y-diputados'))
+ScraperWiki.save_sqlite([:id], twitter_list.people('AsambleaSV', 'diputadas-y-diputados1'))
